a565c2772093eb9f455f9913488f49f33445a2d1
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-05-16 18:00:46 +0530

    Adonetcore

---
+++ ADONETCOREAPP/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ADONETCOREAPP;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("** ALL TRAINERS **");
+
+List<Trainer> trainers = new List<Trainer>();
+
+string connectionString =
+    "server=.\\sqlexpress; database=arbajadonetdb; integrated security=true;encrypt=false;";
+
+SqlConnection con = new SqlConnection(connectionString);
+
+SqlCommand cmd = new SqlCommand("select * from trainer", con);
+con.Open();
+
+SqlDataReader reader= cmd.ExecuteReader();
+
+if (reader.HasRows)
+{
+    while (reader.Read())
+    {
+        Trainer t = new Trainer()
+        {
+            Id = (int)reader["id"],
+            Name =  reader["name"].ToString(),
+            City =  reader["city"].ToString(),
+            experience = (int)reader["experience"]
+        };
+        trainers.Add(t);
+    }
+}
+else
+{
+    trainers = null;
+}
+con.Close();
+ 
+
+
+foreach (var item in trainers)
+{
+    Console.WriteLine($"Id :{item.Id} Name : {item.Name} City :{item.City} Experience :{item.experience}");
+}
+
+ 
+
+
+Console.ReadLine(); 
---
+++ ADONETCOREAPP/Trainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONETCOREAPP
+{
+    public class Trainer
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public int experience { get; set; }
+    }
+}
